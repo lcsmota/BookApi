cfd9e7042032d9e9e2a9f6b9d7f5fe619d6587d3
lcsmota <sacul_m@protonmail.com>
2022-11-22 15:49:49 -0300

    Add book DTO for creation

---
+++ DTOs/BookForCreationDTO.cs
@@ -0,0 +1,10 @@
+namespace BookApi.DTOs;
+public class BookForCreationDTO
+{
+    public string Title { get; set; }
+    public string Publisher { get; set; }
+    public short Pages { get; set; }
+    public string ISBN { get; set; }
+    public DateTime PublishedAt { get; set; }
+    public int AuthorId { get; set; }
+}
