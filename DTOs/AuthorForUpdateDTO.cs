8bace4b076209fc5a9fb2bf4aa456945e9eb2d10
lcsmota <sacul_m@protonmail.com>
2022-11-22 16:09:24 -0300

    Add author DTO for update

---
+++ DTOs/AuthorForUpdateDTO.cs
@@ -0,0 +1,7 @@
+namespace BookApi.DTOs;
+public class AuthorForUpdateDTO
+{
+    public string Name { get; set; }
+    public string Nationality { get; set; }
+    public string Occupation { get; set; }
+}
