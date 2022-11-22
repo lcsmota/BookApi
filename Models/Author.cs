1b2b76be19f36394c4ad73e9bc04451a022af6b2
lcsmota <sacul_m@protonmail.com>
2022-11-22 16:37:44 -0300

    Add attribute JsonIgnore

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,8 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace BookApi.Models;
 public class Author
 {
     public int Id { get; set; }
     public string Name { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Nationality { get; set; }
     public string Occupation { get; set; }
     public List<Book> Books { get; set; } = new();
