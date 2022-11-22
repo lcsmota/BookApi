9069434c1bb560a5d25e5ffbf2925f3a312f686f
lcsmota <sacul_m@protonmail.com>
2022-11-22 16:09:07 -0300

    Add author DTO for creation

---
+++ DTOs/AuthorForCreationDTO.cs
@@ -0,0 +1,7 @@
+namespace BookApi.DTOs;
+public class AuthorForCreationDTO
+{
+    public string Name { get; set; }
+    public string Nationality { get; set; }
+    public string Occupation { get; set; }
+}
